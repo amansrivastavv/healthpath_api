e5f96a7014f4792743fbc19a0254a4a313e0cc85
amansrivastavv <amansrivastav184@gmail.com>
2026-09-25 01:36:43 +0530

    update 6

--- src/main.ts
+++ src/main.ts
@@ -161,7 +161,7 @@ async function bootstrap() {
   });
 
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 
   logger.log(`Application running on: http://localhost:${port}`);
   logger.log(
